d020e7e64090ab9333dd834dcc87c0a4a158df41
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-10-28 15:26:30 -0500

    refactorizing HttpConnection and its working

--- coronavirus-back-end/src/main/java/edu/eci/arsw/coronavirus/services/impl/HttpConnectionServices.java
+++ coronavirus-back-end/src/main/java/edu/eci/arsw/coronavirus/services/impl/HttpConnectionServices.java
@@ -10,27 +10,36 @@ import org.springframework.stereotype.Service;
 /**
  * @author Iván Camilo Rincón Saavedra
  * @version 10/28/2021
+ *
+ * DOCUMENTATION: https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics
  */
 @Service
 public class HttpConnectionServices implements IHttpConnectionServices {
-    private final String URL = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats";
-    private final String APIid= "4f04707326msh95e7ff3629c20d0p119c81jsn52a936c0ca0a";
+    private final String HOST = "covid-19-coronavirus-statistics.p.rapidapi.com";
+    private final String KEY = "4f04707326msh95e7ff3629c20d0p119c81jsn52a936c0ca0a";
+    private final String URL = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total";
 
     public HttpConnectionServices(){
 
     }
 
+    private HttpResponse<String> petition( String url ) throws UnirestException{
+            return Unirest.get( url )
+                    .header("x-rapidapi-host", HOST)
+                    .header("x-rapidapi-key", KEY)
+                    .asString();
+    }
+
     @Override
     public JSONObject getCasesByCountry(String country) throws UnirestException {
-        HttpResponse<String> response = Unirest.get( URL+"?country="+country+"&appid="+APIid ).asString();
-
+        HttpResponse<String> response = petition( URL+"?country="+country );
         JSONObject jsonpObject = new JSONObject(response.getBody());
         return jsonpObject;
     }
 
     @Override
     public JSONObject getAllCases() throws UnirestException {
-        HttpResponse<String> response = Unirest.get( URL+"?appid="+APIid ).asString();
+        HttpResponse<String> response = petition( URL );
         JSONObject jsonpObject = new JSONObject(response.getBody());
         return jsonpObject;
     }
